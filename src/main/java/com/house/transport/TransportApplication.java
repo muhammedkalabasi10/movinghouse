918003f775549fb87774d60a4eed462f09adfb8a
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-08-19 18:12:08 +0300

    customer contract modelleri oluşturuldu

--- src/main/java/com/house/transport/TransportApplication.java
+++ src/main/java/com/house/transport/TransportApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransportApplication {
-
+//This is application context, this application context includes IOC container
 	public static void main(String[] args) {
 		SpringApplication.run(TransportApplication.class, args);
 	}
