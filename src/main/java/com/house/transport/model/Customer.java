c5247bb12111fad55da5c2af741ca208913852d8
dogayldz <dogayyildizz5@outlook.com>
2024-08-27 09:48:42 +0300

    Repositoryler oluşturuldu

---
+++ src/main/java/com/house/transport/model/Customer.java
@@ -0,0 +1,46 @@
+package com.house.transport.model;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    @NotBlank()
+    @Email(message = "Geçerli bir email adresi giriniz")
+    private String email;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    @NotBlank()
+    @Pattern(regexp = "^[0-9]{10}$", message = "Telefon numarası 10 haneli olmalı ve sadece rakamlardan oluşmalıdır")
+    private String phone;
+
+    @Column(name = "password", nullable = false)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password should be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).*$",
+            message = "Password must include at least one letter, one number, and one special character")
+    private String password;
+}
---
+++ src/main/java/com/house/transport/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.house.transport.repository;
+
+import com.house.transport.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
+    Optional<Customer> findByPhone(String phone);
+}
+
