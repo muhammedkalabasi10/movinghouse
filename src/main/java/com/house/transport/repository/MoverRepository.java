1753a6f639ab2616d0f76bde8d5eeb273cec04c7
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-08-27 11:34:38 +0300

    Mover and review repositories added

---
+++ src/main/java/com/house/transport/repository/MoverRepository.java
@@ -0,0 +1,7 @@
+package com.house.transport.repository;
+
+import com.house.transport.model.Mover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoverRepository extends JpaRepository<Mover, Integer> {
+}
