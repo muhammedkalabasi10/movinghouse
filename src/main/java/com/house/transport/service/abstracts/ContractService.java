a91d9409bbdb27c1c1b2aa570e6ec4cf5c5ff2de
dogayldz <dogayyildizz5@outlook.com>
2024-09-04 12:46:48 +0300

    Contract Service created

---
+++ src/main/java/com/house/transport/service/abstracts/ContractService.java
@@ -0,0 +1,19 @@
+package com.house.transport.service.abstracts;
+
+import com.house.transport.model.Contract;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ContractService {
+
+    Contract saveContract(Contract contract);
+
+    Optional<Contract> getContractById(Long id);
+
+    List<Contract> getAllContracts();
+
+    void deleteContractById(Long id);
+
+    List<Contract> getContractsByCustomerId(Long customerId);
+}
