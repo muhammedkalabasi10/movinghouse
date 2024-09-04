1688c2e02da1d6d4c18cd33b18394b4b459ba37a
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-09-04 11:59:04 +0300

    added findbyemail method to mover repository

--- src/main/java/com/house/transport/repository/MoverRepository.java
+++ src/main/java/com/house/transport/repository/MoverRepository.java
@@ -3,5 +3,8 @@ package com.house.transport.repository;
 import com.house.transport.model.Mover;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MoverRepository extends JpaRepository<Mover, Long> {
+    Optional<Mover> findByEmail(String email);
 }
