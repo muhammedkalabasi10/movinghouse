c3b42dae8451c9b4524a2974b4d387058cb59c6f
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-09-12 11:29:42 +0300

    creating and deleting mover removed from service class and interface

--- src/main/java/com/house/transport/service/abstracts/MoverService.java
+++ src/main/java/com/house/transport/service/abstracts/MoverService.java
@@ -5,9 +5,7 @@ import com.house.transport.model.Mover;
 import java.util.List;
 
 public interface MoverService {
-    public abstract Mover createMover(Mover mover);
     public abstract List<Mover> fetchMovers();
     public abstract Mover getMoverById(Long id);
     public abstract Mover updateMover(Mover mover);
-    public abstract boolean deleteMoverById(Long id);
 }
