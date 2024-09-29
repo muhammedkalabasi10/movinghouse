5d444a0b8ed9c5f7de95b9618ab38fb0f665db2b
muhammedkalabasi10 <muhammedkalabasi@gmail.com>
2024-09-29 13:11:03 +0300

    log4j2 configured to save logs to a file

--- src/main/java/com/house/transport/TransportApplication.java
+++ src/main/java/com/house/transport/TransportApplication.java
@@ -1,12 +1,15 @@
 package com.house.transport;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransportApplication {
+	private final static Logger logger = LogManager.getLogger(TransportApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TransportApplication.class, args);
+		logger.info("[TransportApplication][main] Application started...");
 	}
-
 }
